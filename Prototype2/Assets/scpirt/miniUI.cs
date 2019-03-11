cbe8a5c21ee03771ae94e0506cea2328466441a0
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-03-12 11:44:52 +1300

    scoring done

---
+++ Prototype2/Assets/scpirt/miniUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class miniUI : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //testing 
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    score.Nextlevel();
+        //}
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    score.Roll();
+        //}
+        
+
+        transform.GetChild(0).GetComponent<Text>().text = score.levelNumber.ToString();
+        transform.GetChild(1).GetComponent<Text>().text = score.currentLevelScore.ToString();
+    }
+}
---
+++ Prototype2/Assets/scpirt/score.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    public static int[] scoreBoard = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };//9
+    public static int currentLevelScore = 0;
+    public static int levelNumber = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    public static void Nextlevel()
+    {
+        scoreBoard[levelNumber-1] = currentLevelScore;
+        currentLevelScore = 0;
+        levelNumber++;
+    }
+    public static void Roll()
+    {
+        currentLevelScore++;
+    }
+}
