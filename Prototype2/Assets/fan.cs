320a6b97daa6dba405420c86435203ce96722efc
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-03-12 12:05:51 +1300

    speed boost and fan made

---
+++ Prototype2/Assets/fan.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fan : MonoBehaviour
+{
+    public float boost;
+    public Vector3 boostDirection;
+    // Start is called before the first frame update
+
+
+   private void OnTriggerStay(Collider other)
+    {
+        other.gameObject.GetComponent<Rigidbody>().AddForce(boostDirection * boost, ForceMode.Force);
+
+        // other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 18, 0), ForceMode.Acceleration);
+        //other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+    }
+}
