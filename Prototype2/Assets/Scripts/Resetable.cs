c698f9ccd3e526405f3ec24be4ee74be55cbf202
Kerry <k.of.blades@gmail.com>
2019-03-12 08:48:27 +1300

    Changed milestone markers to better show distances Made resettable objects return to their starting rotation and zero their rigidbody velocity (if it exists)

--- Prototype2/Assets/Scripts/Resetable.cs
+++ Prototype2/Assets/Scripts/Resetable.cs
@@ -6,10 +6,12 @@ public class Resetable : MonoBehaviour
 {
 
     private Vector3 startPosition;
+    private Quaternion startRotation;
     // Start is called before the first frame update
     void Start()
     {
         startPosition = transform.position;
+        startRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -18,8 +20,14 @@ public class Resetable : MonoBehaviour
         
     }
 
+    // Returns the object to its original position and rotation and zeroes its velocity 
     public void ResetPosition() {
         transform.position = startPosition;
+        transform.rotation = startRotation;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb) {
+            rb.velocity = Vector3.zero;
+        }
     }
 
     public static void ResetAllPositions() {
