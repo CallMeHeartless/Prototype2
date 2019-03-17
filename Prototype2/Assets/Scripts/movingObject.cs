26054d5a2e4c570fa5499cde34743d93143e332c
Kerry <k.of.blades@gmail.com>
2019-03-18 09:31:35 +1300

    Updated moveObject to allow for rotation and backwards movement

--- Prototype2/Assets/Scripts/movingObject.cs
+++ Prototype2/Assets/Scripts/movingObject.cs
@@ -4,33 +4,85 @@ using UnityEngine;
 
 public class movingObject : MonoBehaviour
 {
+    [Header("Control")]
+    public bool isMoving = false;
+    public bool isRotating = false;
+    public float speed;
+    public float rotationSpeed;
+    private bool movingForwards = true;
+
+    [Header("Targets")]
     public GameObject[] moveTo;
     public Transform goal;
     public int CurrentGoal;
-    public float speed;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         CurrentGoal = 0;
         goal = moveTo[0].transform;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if (Vector3.Distance(transform.position, moveTo[CurrentGoal].transform.position) <= 0.2f)//at loctation
+        //{
+            
+        //    if (CurrentGoal == moveTo.Length-1)
+        //    {
+        //        CurrentGoal = 0;
+        //    }
+        //    else
+        //    {
+        //        CurrentGoal++;
+        //    }
+        //    goal = moveTo[CurrentGoal].transform;
+        //}
+        //transform.position = Vector3.MoveTowards(transform.position, moveTo[CurrentGoal].transform.position, speed * Time.deltaTime);
+    }
+
+    private void FixedUpdate() {
+        if (isMoving) {
+            MoveObject();
+        }
+        if (isRotating) {
+            RotateObject();
+        }
+    }
+
+    private void MoveObject() {
         if (Vector3.Distance(transform.position, moveTo[CurrentGoal].transform.position) <= 0.2f)//at loctation
         {
+            if (movingForwards) {
+                ++CurrentGoal;
+                CheckForEndOfArray();
+            } else {
+                --CurrentGoal;
+                CheckForEndOfArray();
+            }
+
+
             
-            if (CurrentGoal == moveTo.Length-1)
-            {
-                CurrentGoal = 0;
-            }
-            else
-            {
-                CurrentGoal++;
-            }
             goal = moveTo[CurrentGoal].transform;
         }
         transform.position = Vector3.MoveTowards(transform.position, moveTo[CurrentGoal].transform.position, speed * Time.deltaTime);
+    }
+
+    private void RotateObject() {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    private void CheckForEndOfArray() {
+        if (CurrentGoal > moveTo.Length - 1) {
+            CurrentGoal = moveTo.Length - 2;
+            movingForwards = false;
+        } else if (CurrentGoal < 0) {
+            CurrentGoal = 1;
+            movingForwards = true;
+        }
     }
 }
