4cc515359f23a69bd1ffa6b061a98fd284f4176f
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2019-03-18 11:03:47 +1300

    nik traps moved and add a spining platform, spawning logs

---
+++ Prototype2/Assets/spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+    public float MaxTimer;
+    public float Timer;
+    public GameObject WhatToSpawn;
+    public Vector3 MoveObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = MaxTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Timer <= 0)
+        {
+            Timer = MaxTimer;
+            GameObject Item = Instantiate(WhatToSpawn, transform.position, transform.rotation);
+             
+            Item.GetComponentInChildren<Rigidbody>().AddForce(MoveObject, ForceMode.Acceleration);
+
+        }
+        Timer -= Time.deltaTime;
+    }
+}
