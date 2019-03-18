62ae60fc9eb8bf9a84a664568b1a64b623ca957e
Kerry <k.of.blades@gmail.com>
2019-03-19 09:57:41 +1300

    Updated gates that act as waypoints to open a portal

---
+++ Prototype2/Assets/Scripts/GateMaster.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateMaster : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject portal;
+    Transform[] childGates;
+
+    void Start()
+    {
+        childGates = GetComponentsInChildren<Transform>();
+        if (!childGates[0]) {
+            Debug.LogError("ERROR: Child gates are missing from gate master. Null reference exception will follow");
+        }
+        if (!portal) {
+            portal = GameObject.Find("Portal");
+        }
+    }
+
+    public void UpdateGateState() {
+        if (CheckGatesAreClosed()) {
+            portal.SetActive(true);
+        }
+    }
+
+
+    // Checks if all gates have been closed
+    private bool CheckGatesAreClosed() {
+        if (childGates[0]) {
+            foreach (Transform gate in childGates) {
+                if (!gate.GetComponent<GateController>().hasBeenTriggered) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
